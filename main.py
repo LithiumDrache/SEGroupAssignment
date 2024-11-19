5befd6825a9c6a0a94d41c63d28cd51cde8369e1
Cameron <cameron.cartwright122804@gmail.com>
2024-11-19 16:17:25 +0000

    Python file with requirements

---
+++ main.py
@@ -0,0 +1,14 @@
+# Import the required modules
+import torch
+from PIL import Image as PILImage
+import torchvision.transforms as transforms
+import torchvision.models as models
+from flask import Flask, render_template_string, request, redirect, url_for, session
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from email_validator import validate_email, EmailNotValidError
+import re
+import subprocess
+import requests
+import time
+import uuid
